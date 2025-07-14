946667489dc62e747499a675e6cfb1589e3b8a80
Mehran <mehran.naderi8931@gmail.com>
2025-07-14 11:16:27 +0430

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignOut from "./pages/SignOut"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
+import SignUp from "./pages/SignUp"
 
 export default function App() {
   return <BrowserRouter>
@@ -12,7 +13,7 @@ export default function App() {
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignOut />} />
+    <Route path="/sign-up" element={<SignUp />} />
     <Route path="/about" element={<About />} />
     <Route path="/profile" element={<Profile />} />
   </Routes>
