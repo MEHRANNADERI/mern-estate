6363a2ed3a3b84f4e017c65df535d42ee4345992
Mehran <mehran.naderi8931@gmail.com>
2025-07-15 19:11:08 +0430

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,19 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from './user/userSlice';
-import { persistReducer, persistStore } from 'redux-persist';
+import persistReducer from "redux-persist/es/persistReducer";
 import storage from 'redux-persist/lib/storage';
+import persistStore from "redux-persist/es/persistStore";
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({user: userReducer})
 
 const persistConfig = {
   key: 'root',
   storage,
   version: 1,
-};
+}
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
@@ -21,4 +23,20 @@ export const store = configureStore({
     }),
 });
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store)
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import userReducer from './user/userSlice';
+//  // دقت کن: import پیش‌فرض
+
+// export const store = configureStore({
+//   reducer: {
+//     user: userReducer,  // اسم استیت در Redux: user
+//   },
+// });
+
+// // Infer the `RootState` and `AppDispatch` types from the store itself
+// export type RootState = ReturnType<typeof store.getState>;
+// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// export type AppDispatch = typeof store.dispatch;
